9e9b846cb8b13c9d18bfa2c27bcfe2cf487a0b0a
99003754 <78854545+99003754@users.noreply.github.com>
2021-02-17 14:16:42 +0530

    Add files via upload

---
+++ 3_Implementation/src/filetest.c
@@ -0,0 +1,92 @@
+#include"filetest.h"
+
+int main()
+{
+    char path[200];
+    printf("enter path\n");
+    scanf("%s",path);
+    files(path);
+    return 0;
+  }
+    
+void files(char address[200])
+{
+
+    char line[MAX_LINE_LENGTH] = {0};
+    unsigned int line_count = 0;
+     FILE *file = fopen(address, "r");
+    
+    if (!file)
+    {
+        perror(address);
+        return EXIT_FAILURE;
+    }
+    
+    
+    while (fgets(line, MAX_LINE_LENGTH,file))
+    {
+  //  printf("line is %d\n",line);
+        
+        //printf("%s\n",line);
+        function(line);
+
+       
+
+}
+}
+char* function(char input[100])
+{
+int i,j=0,start=0,k=0;
+int temp;
+int end=0;
+char output[100];
+int length=strlen(input);
+
+
+for(i=0;i<length;i++)
+{
+temp=i;
+
+
+if(input[i]=='$')
+{
+temp=i;
+start=i;
+while(input[temp]!=';')
+{
+
+if(input[temp]=='$')
+{
+end=0;
+
+}
+
+
+end++;
+
+
+temp++;
+
+
+
+}
+}
+
+}
+temp=start+end;
+
+printf("$");
+for(j=0;j<temp;j++)
+{
+if(j>start)
+{
+output[k]=input[j];
+//printf("%c",output[k]);
+k++;
+}
+}
+//printf(";");
+//printf("\n");
+return output;
+}
+
